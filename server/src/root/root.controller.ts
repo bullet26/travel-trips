5080820b620f8af421d469f5ffe57c543b3fa52f
Pulna Yuliia <95103780+bullet26@users.noreply.github.com>
2024-11-25 15:37:53 +0200

    add root route

--- server/src/root/root.controller.ts
+++ server/src/root/root.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 import { RootService } from './root.service';
 
-@Controller('root')
+@Controller()
 export class RootController {
   constructor(private readonly rootService: RootService) {}
 
