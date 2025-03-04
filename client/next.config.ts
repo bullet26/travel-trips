db505c5ce1ebef6f324dbfd650e9bad5fd507d21
Pulna Yuliia <95103780+bullet26@users.noreply.github.com>
2025-03-04 15:26:29 +0200

    transform trip, add map

--- client/next.config.ts
+++ client/next.config.ts
@@ -20,19 +20,32 @@ const nextConfig: NextConfig = {
       throw new Error('Webpack config is undefined')
     }
 
-    config.module.rules.push({
-      test: /\.svg$/i,
-      issuer: /\.[jt]sx?$/,
-      use: [
-        {
-          loader: '@svgr/webpack',
-          options: {
-            icon: true,
-            defaultExport: 'component',
+    config.module.rules = config.module.rules.map(
+      (rule: { test: { toString: () => string | string[] } }) => {
+        if (rule && rule.test && rule.test.toString().includes('svg')) {
+          return { ...rule, exclude: /\.svg$/i }
+        }
+        return rule
+      },
+    )
+
+    config.module.rules.push(
+      {
+        test: /\.svg$/i,
+        issuer: /\.[jt]sx?$/,
+        use: [
+          {
+            loader: '@svgr/webpack',
+            options: { icon: true },
           },
-        },
-      ],
-    })
+        ],
+      },
+      {
+        test: /\.svg$/i,
+        issuer: /\.(css|scss|sass)$/,
+        type: 'asset/resource',
+      },
+    )
 
     return config
   },
