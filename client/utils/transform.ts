1eba69dac2b61d179017d75afea1e31685c76ce1
Pulna Yuliia <95103780+bullet26@users.noreply.github.com>
2025-05-10 15:14:11 +0300

    add timezone

--- client/utils/transform.ts
+++ client/utils/transform.ts
@@ -1,11 +1,18 @@
 import dayjs from 'dayjs'
+import utc from 'dayjs/plugin/utc'
+import timezone from 'dayjs/plugin/timezone'
+
+dayjs.extend(utc)
+dayjs.extend(timezone)
+const TZ = 'Europe/Kyiv'
 
 export const formatToDateString = (value: string | Date): string => {
   const date = value instanceof Date ? value : new Date(value)
+  console.log(date, value instanceof Date, value)
 
   if (isNaN(date.getTime())) {
     console.error('Invalid date provided')
   }
 
-  return dayjs(date).format('DD MMMM, YYYY')
+  return dayjs(date).tz(TZ).format('DD MMMM, YYYY')
 }
